9eb7e0f3205653de5f13d42496e5d24f3981e3af
Kavya Sudha <86704648+Kavya0907@users.noreply.github.com>
2025-04-08 00:57:07 -0400

    Delete backend directory

--- backend/src/main/java/com/example/weatherapp/repository/WeatherRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.repository;
-
-import com.example.weatherapp.model.WeatherRequest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface WeatherRequestRepository extends JpaRepository<WeatherRequest, Long> {
-}
